ec4c9d0bd2272d74c2f36bd494caadbc574cae2e
Eysenck <eyscode@gmail.com>
2012-01-18 04:05:21 -0500

    creacion de clase de prueba

---
+++ source/modulo.py
@@ -0,0 +1,17 @@
+'''
+Created on 18/01/2012
+
+@author: eysenck
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
